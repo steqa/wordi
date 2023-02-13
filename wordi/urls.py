b46b9665505eea0c1f26afee314976fef37199b3
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-02-13 17:52:49 +0700

    Add the main wordi application

---
+++ wordi/urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
