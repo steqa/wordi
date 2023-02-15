85740b3405445c1ea887712645fbc29d409f0e63
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-02-15 18:21:55 +0700

    Add tests for the CardImages model

--- cards/tests/test_models.py
+++ cards/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from accounts.models import User
-from cards.models import Card
+from cards.models import Card, CardImages
 from wordi.tests.mixins import TestFieldsParametersValuesMixin
 
 
@@ -44,3 +44,51 @@ class CardTests(TestCase, TestFieldsParametersValuesMixin):
 
     def test_model_verbose_name_plural(self):
         self.assertEqual(Card._meta.verbose_name_plural, 'карточки')
+
+
+class CardImagesTests(TestCase, TestFieldsParametersValuesMixin):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(
+            email='test@gmail.com',
+            first_name='Имя',
+            last_name='Фамилия',
+            password='test12345',
+        )
+        cls.card = Card.objects.create(
+            user=cls.user,
+            front_text='тестовый текст на передней стороне',
+            back_text='тестовый текст на задней стороне',
+        )
+        cls.cardImages = CardImages.objects.create(
+            card=cls.card,
+        )
+        cls.parameters_and_fields_with_value = {
+            'verbose_name': {
+                'card': 'карточка',
+                'front_image': 'изображение на передней стороне',
+                'back_image': 'изображение на задней стороне',
+            },
+            'blank': {
+                'front_image': True,
+                'back_image': True,
+            },
+            'null': {
+                'front_image': True,
+                'back_image': True,
+            }
+        }
+
+    def test_fields_parameters_values(self):
+        super().run_fields_parameters_values_test(CardImages)
+
+    def test_str_method(self):
+        self.assertEqual(str(self.cardImages), str(self.card))
+
+    def test_model_verbose_name(self):
+        self.assertEqual(CardImages._meta.verbose_name,
+                         'изображения карточки')
+
+    def test_model_verbose_name_plural(self):
+        self.assertEqual(CardImages._meta.verbose_name_plural,
+                         'изображения карточек')
