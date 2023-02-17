0e11f10b7d2425c34e31d006422ae3d60ae04e9d
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-02-17 18:39:24 +0700

    Add a display of real cards on the cards page

--- cards/views.py
+++ cards/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import Card, CardImages
+
 
 def cards(request):
-    return render(request, 'cards/cards.html')
+    cards = Card.objects.filter(user=request.user.id)
+    cards_images = CardImages.objects.filter(card__in=cards)
+    context = {
+        'cards': cards,
+        'cards_images': cards_images,
+    }
+    return render(request, 'cards/cards.html', context)
