f58aca4a20f68e4d367625f63a466acc5e954986
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-03-02 18:41:16 +0700

    Add dataclasses for Card and CardImages
    Added dataclasses to validate data when adding a card and card images.

---
+++ cards/data_classes.py
@@ -0,0 +1,25 @@
+from typing import Any, Optional
+
+from pydantic import BaseModel, Field, validator
+
+
+class CardData(BaseModel):
+    front_text: str = Field(alias='formFrontText', min_length=1)
+    back_text: str = Field(alias='formBackText', min_length=1)
+
+
+class CardImagesData(BaseModel):
+    front_image: Optional[list] = Field(alias='formFrontImage')
+    back_image: Optional[list] = Field(alias='formBackImage')
+
+    @ validator('front_image', 'back_image', allow_reuse=True)
+    def test_type(cls, value: list) -> list:
+        if value[0].content_type not in ['image/png', 'image/jpeg']:
+            raise ValueError('Допустимые форматы изображений JPG и PNG.')
+        return value
+
+    @ validator('front_image', 'back_image', allow_reuse=True)
+    def test_max_size(cls, value: list) -> list:
+        if value[0].size > 26214400:
+            raise ValueError('Максимальный размер изображения — 25MB.')
+        return value
