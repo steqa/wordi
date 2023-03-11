3b23750159b09490fd2b513af1bd9a79feedb706
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-03-11 15:00:31 +0700

    Add error display when logging in

--- static/js/accounts/login/send-data.js
+++ static/js/accounts/login/send-data.js
@@ -10,6 +10,7 @@ loginBtn.addEventListener('click', (e) => {
 
 function sendData() {
 	let formData = new FormData();
+	let responseStatus = null;
 	inputs.forEach((input) => {
 		const value = input.value;
 		if (value) {
@@ -22,5 +23,24 @@ function sendData() {
 			'X-CSRFToken': getCookie('csrftoken'),
 		},
 		body: formData,
-	});
+	})
+		.then((response) => {
+			responseStatus = response.status;
+			return response.json();
+		})
+		.then((data) => {
+			if (responseStatus === 200) {
+				localStorage.setItem('toastStatus', 'success');
+				localStorage.setItem('toastMessage', 'Добро пожаловать.');
+				window.location.replace(JSON.parse(data)['redirectUrl']);
+			} else if (responseStatus === 400) {
+				const msg = JSON.parse(data)['msg'];
+				if (msg) {
+					showToast('error', msg);
+				} else {
+					showToast('error', 'Что-то пошло не так.');
+				}
+			}
+		})
+		.catch((error) => console.error(error));
 }
