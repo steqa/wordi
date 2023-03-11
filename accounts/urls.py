df9fce548f2fc4ad677caab728806850643a222f
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-03-11 15:15:44 +0700

    Change the short name for the login URL
    The login URL has also been added to the navigation bar.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_user, name='login_user'),
+    path('login/', views.login_user, name='login-user'),
 ]
