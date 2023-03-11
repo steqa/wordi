d81f525e7084c4dc1507b5d6a3c4ad3afde584da
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-03-11 15:20:37 +0700

    Add tests for login_user views

---
+++ accounts/tests/test_views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from wordi.tests.mixins import (TestPagesShowCorrectContextMixin,
+                                TestPagesUsesCorrectTemplateMixin)
+
+User = get_user_model()
+
+
+class CardsPagesTests(TestCase, TestPagesUsesCorrectTemplateMixin,
+                      TestPagesShowCorrectContextMixin):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(
+            email='test@gmail.com',
+            first_name='Имя',
+            last_name='Фамилия',
+            password='test12345',
+        )
+        cls.authorized_client = Client()
+        cls.authorized_client.force_login(cls.user)
+        cls.templates_pages_names = {
+            'accounts/login.html': reverse('login-user'),
+        }
+
+    def test_pages_uses_correct_template(self):
+        super().run_pages_uses_correct_template_test()
