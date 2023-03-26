b86d412a03ab852d11ce0de11da4d32ca2028fcb
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-03-26 13:35:20 +0700

    Add an answer check to the lesson page

---
+++ static/js/lesson/lesson.js
@@ -0,0 +1,57 @@
+const cards = document.querySelectorAll('.full-card');
+const bg = document.querySelector('.center-outer');
+const nextBtn = document.getElementById('nextBtn');
+let currentCardIndex = 0;
+
+function showNextCard() {
+	nextBtn.classList.add('visually-hidden');
+	nextBtn.classList.remove('btn-success');
+	nextBtn.classList.remove('btn-danger');
+	bg.classList.remove('bg-correct');
+	bg.classList.remove('bg-incorrect');
+	cards[currentCardIndex].classList.add('visually-hidden');
+	currentCardIndex++;
+	if (currentCardIndex >= cards.length) {
+		console.log('END');
+	} else {
+		cards[currentCardIndex].classList.remove('visually-hidden');
+	}
+	console.log(cards);
+}
+
+const submitBtns = document.querySelectorAll('.submit-btn');
+submitBtns.forEach((submitBtn) => {
+	submitBtn.addEventListener('click', (event) => {
+		console.log('click');
+		const currentCard = cards[currentCardIndex];
+		const answer = currentCard
+			.querySelector('input[type="text"]')
+			.value.trim()
+			.toLowerCase();
+		const correctAnswer = currentCard
+			.querySelector('input[type="hidden"]')
+			.value.trim()
+			.toLowerCase();
+
+		if (answer === correctAnswer) {
+			currentCard.classList.add('correct');
+			nextBtn.classList.add('btn-success');
+			bg.classList.add('bg-correct');
+		} else {
+			currentCard.classList.add('incorrect');
+			currentCard
+				.querySelector('.back-side')
+				.classList.remove('visually-hidden');
+			nextBtn.classList.add('btn-danger');
+			bg.classList.add('bg-incorrect');
+		}
+		submitBtn.classList.add('visually-hidden');
+		nextBtn.classList.remove('visually-hidden');
+	});
+});
+
+nextBtn.addEventListener('click', (e) => {
+	showNextCard();
+});
+
+cards[currentCardIndex].classList.remove('visually-hidden');
