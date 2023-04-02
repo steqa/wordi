6debf700bbfa682f1cf8c139ca8d2d35e9a660b0
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-04-02 14:00:31 +0700

    Add a temporary storage of statistics data
    Data for each year is now stored in a variable
    and is requested once when the year changes.
    The data is requested again when the page is refreshed.

--- static/js/stats/get-calendar-data.js
+++ static/js/stats/get-calendar-data.js
@@ -1,4 +1,13 @@
+let temporaryStorage = {};
+
 function getCalendarData() {
+	if (selectedYear in temporaryStorage) {
+		return new Promise((resolve, reject) => {
+			data = temporaryStorage[selectedYear];
+			const dataSource = formCalendarData(temporaryStorage[selectedYear]);
+			resolve(dataSource);
+		});
+	}
 	let responseStatus = null;
 	return fetch(window.location.href + `?year=${selectedYear}`, {
 		method: 'GET',
@@ -11,30 +20,37 @@ function getCalendarData() {
 			return response.json();
 		})
 		.then((data) => {
-			const calendarDataSource = [];
 			if (responseStatus === 200) {
-				const calendarData = data.calendarData;
-				for (let date in calendarData) {
-					const color = '#0dcaf0';
-					const correctAnswers = calendarData[date];
-					let startDate = new Date(date);
-					let endDate = new Date(date);
-
-					calendarDataSource.push({
-						color: color,
-						correctAnswers: correctAnswers,
-						startDate: startDate,
-						endDate: endDate,
-					});
-				}
+				data = data.calendarData;
+				const dataSource = formCalendarData(data);
+				return dataSource;
 			} else if (responseStatus === 400) {
 				showToast('error', 'Не удалось загрузить статистику.');
+				return [];
 			}
-			return calendarDataSource;
 		})
 		.catch((error) => {
 			showToast('error', 'Не удалось загрузить статистику.');
 			console.error(error);
 			return [];
 		});
+}
+
+function formCalendarData(data) {
+	const calendarDataSource = [];
+	temporaryStorage[selectedYear] = data;
+	for (let date in data) {
+		const color = '#0dcaf0';
+		const correctAnswers = data[date];
+		let startDate = new Date(date);
+		let endDate = new Date(date);
+
+		calendarDataSource.push({
+			color: color,
+			correctAnswers: correctAnswers,
+			startDate: startDate,
+			endDate: endDate,
+		});
+	}
+	return calendarDataSource;
 }
